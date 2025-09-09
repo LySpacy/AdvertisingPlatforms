f8af05504bff8715e62c3e3319e08c36bebddf81
Vladimir <mrdontofgaming@gmail.com>
2025-09-09 20:12:53 +0300

    domain

---
+++ AdvertisingPlatforms.Domain/Interfaces/IAdPlatformRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace AdvertisingPlatforms.Domain.Interfaces;
+
+public interface IAdPlatformRepository
+{
+    /// <summary>
+    /// Загрузка данных из строк
+    /// </summary>
+    /// <param name="lines">Газеты с их локациями работы</param>
+    void LoadFromLines(IEnumerable<string> lines);
+    
+    /// <summary>
+    /// Поиск газет по локации
+    /// </summary>
+    /// <param name="location">индекс локации</param>
+    /// <returns>Список газет</returns>
+    List<string> FindPlatforms(string location);
+}
---
+++ AdvertisingPlatforms.Domain/Interfaces/IAdPlatformService.cs
@@ -0,0 +1,21 @@
+﻿using AdvertisingPlatforms.Domain.DTO;
+using Microsoft.AspNet.Http;
+
+namespace AdvertisingPlatforms.Domain.Interfaces;
+
+public interface IAdPlatformService
+{
+    /// <summary>
+    /// Асинхронная выгрузка строк из файла
+    /// </summary>
+    /// <param name="file">Файл</param>
+    /// <param name="cancellationToken">Токен отмены</param>
+    Task LoadAsync(IFormFile file, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Поиск газет по индексу локации
+    /// </summary>
+    /// <param name="location">Индекс локации</param>
+    /// <returns>Список газет</returns>
+    IEnumerable<AdPlatform> Search(string location);
+}
