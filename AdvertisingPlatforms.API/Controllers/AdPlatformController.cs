7ac510a19615a32da653b8a62c13f646ff08d3ea
Vladimir <mrdontofgaming@gmail.com>
2025-09-09 22:11:09 +0300

    Добавлены комментарии в слой апи

--- AdvertisingPlatforms.API/Controllers/AdPlatformController.cs
+++ AdvertisingPlatforms.API/Controllers/AdPlatformController.cs
@@ -10,11 +10,21 @@ public class AdPlatformController : ControllerBase
 {
     private readonly IAdPlatformService _service;
 
+    /// <summary>
+    /// Конструктор контроллера.
+    /// </summary>
+    /// <param name="service">Сервис для работы с рекламными платформами.</param>
     public AdPlatformController(IAdPlatformService service)
     {
         _service = service;
     }
 
+    /// <summary>
+    /// Загрузка файла с данными рекламных платформ и их локаций.
+    /// </summary>
+    /// <param name="file">Файл для загрузки.</param>
+    /// <param name="cancellationToken">Токен отмены операции.</param>
+    /// <returns>Возвращает статус выполнения операции.</returns>
     [HttpPost("load")]
     public async Task<IActionResult> Load(IFormFile file,
         CancellationToken cancellationToken)
@@ -26,6 +36,11 @@ public class AdPlatformController : ControllerBase
         return Ok("Файл загружен");
     }
 
+    /// <summary>
+    /// Поиск рекламных платформ по указанной локации.
+    /// </summary>
+    /// <param name="location">Локация для поиска (например, "/ru/msk").</param>
+    /// <returns>Список наименований подходящих рекламных платформ.</returns>
     [HttpGet("search")]
     public IActionResult Search([FromQuery] string location)
     {
